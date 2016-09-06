703f55777bed66a1ab4f49f560a75ede41e90ada
mithun <mithunf@qburst.com>
2016-09-06 11:07:24 +0530

    hover added to aside section

--- js/json.js
+++ js/json.js
@@ -7,3 +7,17 @@ var articles = [
 		 "heading": "Apple's App Store Is About to Get Soooooooo Much Better"},
 	];
 $("#scrollerTemplate").tmpl(articles).appendTo(".image-scroller");
+
+
+var mainArticles = [
+		{
+		"heading": "Intel Lawyers Tell John McAfee He Can't Call His Company John McAfee",
+		"author": "Hudson Hongo",
+		"time": "29 minutes ago",
+		"commentImage": "images/2-512.png",
+		"ratingImage": "images/download.png",
+		"mainImage": "images/teguqjqe9gcylkja79ry.jpg",
+		"para": "Sometimes, all a man has in this world is his name, but according to Intel,antivirus pioneer and living cautionary tale John McAfee "
+		}
+];
+$("#contentArticleTemplate").tmpl(mainArticles).appendTo(".articles-container");
